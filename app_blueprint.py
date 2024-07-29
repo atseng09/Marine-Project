4b01d8b4e59dbda33be790096d5a78a184965869
Alison Tseng <alisontseng@Alisons-MacBook-Pro.local>
2024-07-29 17:56:23 -0400

    initial commit

---
+++ app_blueprint.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, render_template
+
+app_blueprint = Blueprint('app_blueprint',__name__)
+
+@app_blueprint.route("/")
+def home():
+    return render_template("homepage.html")
+
+def root():
+   markers=[
+   {
+   'lat':0,
+   'lon':0,
+   'popup':'This is the middle of the map.'
+    }
+   ]
+   return render_template('homepage.html',markers=markers )
+
+
+# @app_blueprint.route('/about')
+# def about():
+#     return "About"
